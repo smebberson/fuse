1fb6b525ba2d788bfb291f96e8ccde58b02cbbfc
Scott Mebberson <scott@scottmebberson.com>
2011-11-13 20:25:04 +1030

    Fuse will now not only watch the input file (when using -w) but also any referenced files

--- bin/fuse.js
+++ bin/fuse.js
@@ -1,15 +1,29 @@
 #!/usr/bin/env node
 
-var argv = require('optimist').usage('Usage: $0 -i [javascript-file.js] -o [javascript-file.js]').demand(['i', 'o']).argv;
+var fs = require('fs');
+var _ = require('underscore');
+var argv = require('optimist').usage('Usage: $0 -i [javascript-file.js] -o [javascript-file.js] (-w)').demand(['i', 'o']).describe('i', 'JavaScript input file').describe('o', 'JavaScript output file').describe('w', 'Watch the input file for changes.').argv;
 var fuse = require('../lib/fuse');
 
 if (argv.w) {
 	
+	// let's grab each file that we need to watch
+	var a = fuse.getReferencedFiles(fuse.getFileContent(argv.i));
+	var relativePath = fuse.getFilePath(argv.i);
+	
+	// loop through an setup a watch on each referenced file
+	_(a).each(function (path) {
+		fuse.watchSrcFile(relativePath + path.path, argv.i, argv.o);
+		console.log('Watching ' + path.path + ' for changes.');
+	});
+	
+	// we also need to watch the input file
 	fuse.watchFile(argv.i, argv.o);
+	console.log('Watching ' + argv.i + ' for changes.\nCTRL + C to stop.');
 	
 } else {
 
 	// assume we're not watching a file, let's just package!
-	fuse.readFile(argv.i, argv.o);
+	fuse.fuseFile(argv.i, argv.o);
 
 }
