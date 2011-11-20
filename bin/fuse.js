d8cb542857e8f0e8d471965252eb42ffc038c5c9
Scott Mebberson <smebberson@warhol.fritz.box>
2011-11-20 22:27:36 +1030

    Added colouring to the output and improved output when no @depends directives were found

--- bin/fuse.js
+++ bin/fuse.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var colors = require('colors');
 var fs = require('fs');
 var _ = require('underscore');
 var argv = require('optimist').usage('Usage: $0 -i [javascript-file.js] -o [javascript-file.js] (-w)').demand(['i', 'o']).describe('i', 'JavaScript input file').describe('o', 'JavaScript output file').describe('w', 'Watch the input file for changes.').argv;
@@ -14,12 +15,12 @@ if (argv.w) {
 	// loop through an setup a watch on each referenced file
 	_(a).each(function (path) {
 		fuse.watchSrcFile(relativePath + path.path, argv.i, argv.o);
-		console.log('Watching ' + path.path + ' for changes.');
+		console.log('Watching ' + colors.cyan( path.path ) + ' for changes.');
 	});
 	
 	// we also need to watch the input file
 	fuse.watchFile(argv.i, argv.o);
-	console.log('Watching ' + argv.i + ' for changes.\nCTRL + C to stop.');
+	console.log('Watching ' + colors.cyan( argv.i ) + ' for changes.\nCTRL + C to stop.');
 	
 } else {
 
