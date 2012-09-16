f8df9940e031bfbd889258770a9168a09a1b3ca0
Scott Mebberson <scott@scottmebberson.com>
2012-09-16 14:31:59 +0930

    Added a bunch of unit tests

---
+++ test/result/twoDepends/output.js
@@ -0,0 +1,7 @@
+
+// this is the content of input.js and should be on the second line
+
+// this is the one and only line in depends-one.js
+
+
+// this is the last line of input.js
---
+++ test/src/twoDepends/input.js
@@ -0,0 +1,6 @@
+
+// this is the content of input.js and should be on the second line
+
+// @depends depends-one.js
+
+// this is the last line of input.js
