6275f53410a77289b36c391332f37455bf796c90
Scott Mebberson <scott@scottmebberson.com>
2013-07-17 13:36:29 +0930

    Updated the test suite such that directories and files are now created and removed after each test run.

--- test/test.js
+++ test/test.js
@@ -10,9 +10,22 @@ describe('Using fuse', function () {
 
 		describe('with javascript', function () {
 
+			before(function (done){
+				// make the directory first to hold the result content
+				fs.mkdir(process.cwd() + '/test/javascript/result/', done);
+			});
+
+			after(function (done) {
+				// remove the result directory
+				fs.rmdir(process.cwd() + '/test/javascript/result/', done);
+			});
+
 			describe('should fuse two files', function () {
 
 				it('by @depends', function (done) {
+
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/javascript/result/depends/');
 
 					exec('fuse -i ' + process.cwd() + '/test/javascript/src/depends/basic-depends.js -o ' + process.cwd() + '/test/javascript/result/depends/basic-depends-output.js', function (error, stdout, stderr) {
 
@@ -21,6 +34,7 @@ describe('Using fuse', function () {
 
 						// delete the file
 						fs.unlinkSync(process.cwd() + '/test/javascript/result/depends/basic-depends-output.js');
+						fs.rmdirSync(process.cwd() + '/test/javascript/result/depends/');
 						
 						// we're done
 						done();
@@ -31,6 +45,9 @@ describe('Using fuse', function () {
 
 				it('by @import', function (done) {
 
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/javascript/result/import/');
+
 					exec('fuse -i ' + process.cwd() + '/test/javascript/src/import/basic-import.js -o ' + process.cwd() + '/test/javascript/result/import/basic-import-output.js', function (error, stdout, stderr) {
 
 						// check the output against the expected output
@@ -38,6 +55,7 @@ describe('Using fuse', function () {
 
 						// delete the file
 						fs.unlinkSync(process.cwd() + '/test/javascript/result/import/basic-import-output.js');
+						fs.rmdirSync(process.cwd() + '/test/javascript/result/import/');
 						
 						// we're done
 						done();
@@ -52,6 +70,9 @@ describe('Using fuse', function () {
 
 				it('with two depends', function (done) {
 
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/javascript/result/twoDepends/');
+
 					exec('fuse -i ' + process.cwd() + '/test/javascript/src/twoDepends/input.js -o ' + process.cwd() + '/test/javascript/result/twoDepends/output.js', function (error, stdout, stderr) {
 
 						// check the output against the expected output
@@ -59,6 +80,7 @@ describe('Using fuse', function () {
 
 						// delete the file
 						fs.unlinkSync(process.cwd() + '/test/javascript/result/twoDepends/output.js');
+						fs.rmdirSync(process.cwd() + '/test/javascript/result/twoDepends/');
 						
 						// we're done
 						done();
@@ -73,9 +95,22 @@ describe('Using fuse', function () {
 
 		describe('with html', function () {
 
+			before(function (done){
+				// make the directory first to hold the result content
+				fs.mkdir(process.cwd() + '/test/html/result/', done);
+			});
+
+			after(function (done) {
+				// remove the result directory
+				fs.rmdir(process.cwd() + '/test/html/result/', done);
+			});
+
 			describe('should fuse two files', function () {
 
 				it('by <!-- @depends -->', function (done) {
+
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/html/result/depends/');
 
 					exec('fuse -i ' + process.cwd() + '/test/html/src/depends/basic-depends.html -o ' + process.cwd() + '/test/html/result/depends/basic-depends-output.html', function (error, stdout, stderr) {
 
@@ -84,6 +119,7 @@ describe('Using fuse', function () {
 
 						// delete the file
 						fs.unlinkSync(process.cwd() + '/test/html/result/depends/basic-depends-output.html');
+						fs.rmdirSync(process.cwd() + '/test/html/result/depends/');
 						
 						// we're done
 						done();
@@ -94,6 +130,9 @@ describe('Using fuse', function () {
 
 				it('by <!-- @import -->', function (done) {
 
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/html/result/import/');
+
 					exec('fuse -i ' + process.cwd() + '/test/html/src/import/basic-import.html -o ' + process.cwd() + '/test/html/result/import/basic-import-output.html', function (error, stdout, stderr) {
 
 						// check the output against the expected output
@@ -101,6 +140,7 @@ describe('Using fuse', function () {
 
 						// delete the file
 						fs.unlinkSync(process.cwd() + '/test/html/result/import/basic-import-output.html');
+						fs.rmdirSync(process.cwd() + '/test/html/result/import/');
 
 						// we're done
 						done();
@@ -115,13 +155,17 @@ describe('Using fuse', function () {
 
 				it('with two depends', function (done) {
 
+					// make the directory first to hold the result content
+					fs.mkdirSync(process.cwd() + '/test/html/result/twoDepends/');
+
 					exec('fuse -i ' + process.cwd() + '/test/html/src/twoDepends/input.html -o ' + process.cwd() + '/test/html/result/twoDepends/output.html', function (error, stdout, stderr) {
 
 						// check the output against the expected output
 						assert.equal(fs.readFileSync(process.cwd() + '/test/html/result/twoDepends/output.html', 'utf-8'), fs.readFileSync(process.cwd() + '/test/html/expected/twoDepends/result.html', 'utf-8'));
 
-						// delete the file
+						// delete the file and folder
 						fs.unlinkSync(process.cwd() + '/test/html/result/twoDepends/output.html');
+						fs.rmdirSync(process.cwd() + '/test/html/result/twoDepends');
 
 						// we're done
 						done();
