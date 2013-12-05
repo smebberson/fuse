a9a96a4a24280f5754cf8bcae38b9836f45b28b1
Scott Mebberson <scott@scottmebberson.com>
2013-12-05 23:39:10 +1030

    Updated the test cases to make them more robust (result files no longer live in repo).

--- test/javascript/result/lint/lint.js
+++
@@ -1,5 +0,0 @@
-var a = ( 1 || 2)
-var b = a;
-
-var a = 1;
-var b = 2;
--- test/test.js
+++ test/test.js
@@ -7,14 +7,31 @@ describe('Using fuse', function () {
 
 	describe('with javascript', function () {
 
+		before(function (done){
+			// make the directory first to hold the result content
+			fs.mkdir(process.cwd() + '/test/javascript/result/', done);
+		});
+
+		after(function (done) {
+			// remove the result directory
+			fs.rmdir(process.cwd() + '/test/javascript/result/', done);
+		});
+
 		describe('should fuse two files', function () {
 
 			it('by @depends', function (done) {
+
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/javascript/result/depends/');
 
 				exec('fuse -i ' + process.cwd() + '/test/javascript/src/depends/basic-depends.js -o ' + process.cwd() + '/test/javascript/result/depends/basic-depends-output.js', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/javascript/result/depends/basic-depends-output.js', 'utf-8'), fs.readFileSync(process.cwd() + '/test/javascript/expected/depends/basic-depends-result.js', 'utf-8'));
+
+					// delete the file
+					fs.unlinkSync(process.cwd() + '/test/javascript/result/depends/basic-depends-output.js');
+					fs.rmdirSync(process.cwd() + '/test/javascript/result/depends/');
 
 					// we're done
 					done();
@@ -25,10 +42,17 @@ describe('Using fuse', function () {
 
 			it('by @import', function (done) {
 
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/javascript/result/import/');
+
 				exec('fuse -i ' + process.cwd() + '/test/javascript/src/import/basic-import.js -o ' + process.cwd() + '/test/javascript/result/import/basic-import-output.js', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/javascript/result/import/basic-import-output.js', 'utf-8'), fs.readFileSync(process.cwd() + '/test/javascript/expected/import/basic-import-result.js', 'utf-8'));
+
+					// delete the file
+					fs.unlinkSync(process.cwd() + '/test/javascript/result/import/basic-import-output.js');
+					fs.rmdirSync(process.cwd() + '/test/javascript/result/import/');
 
 					// we're done
 					done();
@@ -43,10 +67,17 @@ describe('Using fuse', function () {
 
 			it('with two depends', function (done) {
 
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/javascript/result/twoDepends/');
+
 				exec('fuse -i ' + process.cwd() + '/test/javascript/src/twoDepends/input.js -o ' + process.cwd() + '/test/javascript/result/twoDepends/output.js', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/javascript/result/twoDepends/output.js', 'utf-8'), fs.readFileSync(process.cwd() + '/test/javascript/expected/twoDepends/result.js', 'utf-8'));
+
+					// delete the file
+					fs.unlinkSync(process.cwd() + '/test/javascript/result/twoDepends/output.js');
+					fs.rmdirSync(process.cwd() + '/test/javascript/result/twoDepends/');
 
 					// we're done
 					done();
@@ -61,15 +92,32 @@ describe('Using fuse', function () {
 
 	describe('with html', function () {
 
+		before(function (done){
+			// make the directory first to hold the result content
+			fs.mkdir(process.cwd() + '/test/html/result/', done);
+		});
+
+		after(function (done) {
+			// remove the result directory
+			fs.rmdir(process.cwd() + '/test/html/result/', done);
+		});
+
 		describe('should fuse two files', function () {
 
 			it('by <!-- @depends -->', function (done) {
+
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/html/result/depends/');
 
 				exec('fuse -i ' + process.cwd() + '/test/html/src/depends/basic-depends.html -o ' + process.cwd() + '/test/html/result/depends/basic-depends-output.html', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/html/result/depends/basic-depends-output.html', 'utf-8'), fs.readFileSync(process.cwd() + '/test/html/expected/depends/basic-depends-result.html', 'utf-8'));
 
+					// delete the file
+					fs.unlinkSync(process.cwd() + '/test/html/result/depends/basic-depends-output.html');
+					fs.rmdirSync(process.cwd() + '/test/html/result/depends/');
+					
 					// we're done
 					done();
 
@@ -79,10 +127,17 @@ describe('Using fuse', function () {
 
 			it('by <!-- @import -->', function (done) {
 
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/html/result/import/');
+
 				exec('fuse -i ' + process.cwd() + '/test/html/src/import/basic-import.html -o ' + process.cwd() + '/test/html/result/import/basic-import-output.html', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/html/result/import/basic-import-output.html', 'utf-8'), fs.readFileSync(process.cwd() + '/test/html/expected/import/basic-import-result.html', 'utf-8'));
+
+					// delete the file
+					fs.unlinkSync(process.cwd() + '/test/html/result/import/basic-import-output.html');
+					fs.rmdirSync(process.cwd() + '/test/html/result/import/');
 
 					// we're done
 					done();
@@ -97,10 +152,17 @@ describe('Using fuse', function () {
 
 			it('with two depends', function (done) {
 
+				// make the directory first to hold the result content
+				fs.mkdirSync(process.cwd() + '/test/html/result/twoDepends/');
+
 				exec('fuse -i ' + process.cwd() + '/test/html/src/twoDepends/input.html -o ' + process.cwd() + '/test/html/result/twoDepends/output.html', function (error, stdout, stderr) {
 
 					// check the output against the expected output
 					assert.equal(fs.readFileSync(process.cwd() + '/test/html/result/twoDepends/output.html', 'utf-8'), fs.readFileSync(process.cwd() + '/test/html/expected/twoDepends/result.html', 'utf-8'));
+
+					// delete the file and folder
+					fs.unlinkSync(process.cwd() + '/test/html/result/twoDepends/output.html');
+					fs.rmdirSync(process.cwd() + '/test/html/result/twoDepends');
 
 					// we're done
 					done();
